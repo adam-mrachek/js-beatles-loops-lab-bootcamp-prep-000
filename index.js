73accb993697dc9e520909f7b2d87abf5fecc29f
Adam Mrachek <amrachek@outlook.com>
2018-03-23 12:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-  
+  var members = [];
+  for (var i = 0; i < musicians.length; i++) {
+    members.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return members;
 }
