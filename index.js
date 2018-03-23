490293a047a1bb8d7fc10f5db4d88f5f229ffdfa
Adam Mrachek <amrachek@outlook.com>
2018-03-23 12:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return members;
 }
+
+function johnLennonFacts(facts) {
+  var loudFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    loudFacts.push(`${facts} + !!!`);
+  }
+  return loudFacts;
+}
